e4f1b4e64c4a47285deafab7513d79df73cd81d7
bageus <36338626+bageus@users.noreply.github.com>
2026-07-14 18:43:48 +0200

    Add agent movement event

---
+++ src/Dig.Domain/Agents/AgentMovementEvents.cs
@@ -0,0 +1,29 @@
+using Dig.Domain.Core;
+using Dig.Domain.World;
+
+namespace Dig.Domain.Agents
+{
+
+public sealed class AgentMoved : IDomainEvent
+{
+    public AgentMoved(
+        long tick,
+        EntityId agentId,
+        CellId previousPosition,
+        CellId currentPosition)
+    {
+        Tick = tick;
+        AgentId = agentId;
+        PreviousPosition = previousPosition;
+        CurrentPosition = currentPosition;
+    }
+
+    public long Tick { get; }
+
+    public EntityId AgentId { get; }
+
+    public CellId PreviousPosition { get; }
+
+    public CellId CurrentPosition { get; }
+}
+}
