95918c27921e5ec9aab6e985313b9e7d1836bd01
bageus <36338626+bageus@users.noreply.github.com>
2026-07-14 02:36:16 +0200

    buildings: add in-memory repository

---
+++ src/Dig.Infrastructure/InMemory/InMemoryBuildingsRepository.cs
@@ -0,0 +1,24 @@
+using Dig.Application.Buildings;
+using Dig.Domain.Buildings;
+
+namespace Dig.Infrastructure.InMemory;
+
+public sealed class InMemoryBuildingsRepository : IBuildingsRepository
+{
+    private BuildingsState _buildings;
+
+    public InMemoryBuildingsRepository(BuildingsState? buildings = null)
+    {
+        _buildings = buildings ?? new BuildingsState();
+    }
+
+    public BuildingsState Get()
+    {
+        return _buildings;
+    }
+
+    public void Save(BuildingsState buildings)
+    {
+        _buildings = buildings ?? throw new ArgumentNullException(nameof(buildings));
+    }
+}
