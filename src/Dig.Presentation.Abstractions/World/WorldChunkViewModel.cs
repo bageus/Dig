cdeec353f745634249b3f8511f4bea4a2015701b
bageus <36338626+bageus@users.noreply.github.com>
2026-07-14 15:47:28 +0200

    Add world chunk view model

---
+++ src/Dig.Presentation.Abstractions/World/WorldChunkViewModel.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace Dig.Presentation.World
+{
+
+public sealed class WorldChunkViewModel
+{
+    public WorldChunkViewModel(
+        int x,
+        int y,
+        long version,
+        IReadOnlyCollection<WorldCellViewModel> cells)
+    {
+        if (version < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(version));
+        }
+
+        if (cells is null)
+        {
+            throw new ArgumentNullException(nameof(cells));
+        }
+
+        X = x;
+        Y = y;
+        Version = version;
+        Cells = new ReadOnlyCollection<WorldCellViewModel>(cells
+            .OrderBy(cell => cell.Y)
+            .ThenBy(cell => cell.X)
+            .ToArray());
+    }
+
+    public int X { get; }
+    public int Y { get; }
+    public long Version { get; }
+    public IReadOnlyList<WorldCellViewModel> Cells { get; }
+}
+}
