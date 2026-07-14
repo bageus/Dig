4f4421c002a36f6e4d85f07ac2aa285383d5a6d6
bageus <36338626+bageus@users.noreply.github.com>
2026-07-14 12:50:06 +0200

    Add allocation-light storage inspection

---
+++ src/Dig.Domain/Storage/StorageInspection.cs
@@ -0,0 +1,22 @@
+namespace Dig.Domain.Storage;
+
+public interface IStorageInspectionVisitor
+{
+    void VisitStorageReservation(StorageReservationSnapshot reservation);
+}
+
+public sealed partial class StorageState
+{
+    public void VisitInspection(IStorageInspectionVisitor visitor)
+    {
+        if (visitor is null)
+        {
+            throw new ArgumentNullException(nameof(visitor));
+        }
+
+        foreach (StorageReservationSnapshot reservation in _reservations.Values)
+        {
+            visitor.VisitStorageReservation(reservation);
+        }
+    }
+}
