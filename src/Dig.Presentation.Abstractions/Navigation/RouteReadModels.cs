908a32a714e52a06028f36d185abcdd38ebb06d4
bageus <36338626+bageus@users.noreply.github.com>
2026-07-14 23:37:18 +0200

    Add route read models

---
+++ src/Dig.Presentation.Abstractions/Navigation/RouteReadModels.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace Dig.Presentation.Navigation
+{
+
+public readonly struct RouteCellViewModel
+{
+    public RouteCellViewModel(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+}
+
+public sealed class RouteViewModel
+{
+    public RouteViewModel(
+        string jobId,
+        string agentId,
+        int workX,
+        int workY,
+        bool succeeded,
+        string detail,
+        int totalCost,
+        long navigationVersion,
+        IReadOnlyCollection<RouteCellViewModel> cells)
+    {
+        JobId = jobId;
+        AgentId = agentId;
+        WorkX = workX;
+        WorkY = workY;
+        Succeeded = succeeded;
+        Detail = detail;
+        TotalCost = totalCost;
+        NavigationVersion = navigationVersion;
+        Cells = new ReadOnlyCollection<RouteCellViewModel>(cells.ToArray());
+    }
+
+    public string JobId { get; }
+    public string AgentId { get; }
+    public int WorkX { get; }
+    public int WorkY { get; }
+    public bool Succeeded { get; }
+    public string Detail { get; }
+    public int TotalCost { get; }
+    public long NavigationVersion { get; }
+    public IReadOnlyList<RouteCellViewModel> Cells { get; }
+}
+
+}
