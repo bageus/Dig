edb48b061b37cc5afdc46a26a6c9ca7336b13de1
bageus <36338626+bageus@users.noreply.github.com>
2026-07-15 18:10:49 +0200

    Add shared manual and autosave application service

---
+++ src/Dig.Application/Saving/SaveGameService.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using Dig.Domain.Core;
+using Dig.Domain.Inventory;
+using Dig.Domain.World;
+
+namespace Dig.Application.Saving
+{
+
+public sealed class SaveGameService
+{
+    private readonly SaveGameBuilder _builder;
+    private readonly SaveGameLoader _loader;
+    private readonly ISaveSlotStore _store;
+
+    public SaveGameService(
+        SaveGameBuilder builder,
+        SaveGameLoader loader,
+        ISaveSlotStore store)
+    {
+        _builder = builder ?? throw new ArgumentNullException(nameof(builder));
+        _loader = loader ?? throw new ArgumentNullException(nameof(loader));
+        _store = store ?? throw new ArgumentNullException(nameof(store));
+    }
+
+    public SaveGameDocument Save(SaveGameContext context)
+    {
+        SaveGameDocument document = _builder.Build(context);
+        _store.Save(document.Metadata.SlotId, document);
+        return document;
+    }
+
+    public SaveGameDocument Autosave(SaveGameContext context)
+    {
+        if (context is null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        SaveMetadataData metadata = new SaveMetadataData
+        {
+            SlotId = SaveSlotNames.Autosave,
+            DisplayName = "Autosave",
+            SavedAtUtc = context.Metadata.SavedAtUtc,
+            SimulationTick = context.Metadata.SimulationTick,
+            WorldSeed = context.Metadata.WorldSeed,
+            GeneratorVersion = context.Metadata.GeneratorVersion,
+        };
+        return Save(new SaveGameContext(
+            metadata,
+            context.World,
+            context.Inventory,
+            context.Jobs));
+    }
+
+    public Result<LoadedGameState> Load(
+        string slotId,
+        MaterialCatalog materials,
+        ItemCatalog items)
+    {
+        SaveGameDocument document = _store.Load(slotId);
+        return _loader.Load(document, materials, items);
+    }
+
+    public IReadOnlyList<SaveSlotInfo> ListSlots()
+    {
+        return _store.List();
+    }
+}
+}
