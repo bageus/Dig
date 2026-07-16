67b883193fb95694d65540f1e9a9039720d0a255
bageus <36338626+bageus@users.noreply.github.com>
2026-07-16 22:09:10 +0200

    add equipment work kinds

--- src/Dig.Domain/Inventory/EquipmentProfiles.cs
+++ src/Dig.Domain/Inventory/EquipmentProfiles.cs
@@ -1,9 +1,17 @@
 namespace Dig.Domain.Inventory
 {
+
 public enum EquipmentAppearanceKind
 {
     Generic = 0,
     Mining = 1,
     Construction = 2,
 }
+
+public enum EquipmentWorkKind
+{
+    Mining = 0,
+    Construction = 1,
+}
+
 }
