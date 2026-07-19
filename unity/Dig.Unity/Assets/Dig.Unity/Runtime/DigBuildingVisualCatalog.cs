8f225c403f5f25dfbf49e3c264ae8b79208e757a
bageus <36338626+bageus@users.noreply.github.com>
2026-07-19 21:03:09 +0200

    Add building visual catalog type

---
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigBuildingVisualCatalog.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Dig.Unity
+{
+    [CreateAssetMenu(
+        fileName = "BuildingVisualCatalog",
+        menuName = "Dig/Visual Catalogs/Buildings")]
+    public sealed class DigBuildingVisualCatalog : DigVisualCatalog
+    {
+    }
+}
