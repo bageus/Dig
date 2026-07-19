f8f6b5b0ff48fa38beec1c5e101f18d47f6c4ad6
bageus <36338626+bageus@users.noreply.github.com>
2026-07-19 22:15:52 +0200

    Add terrain chunk mesh data

---
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainChunkMeshData.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+namespace Dig.Unity
+{
+    internal sealed class DigTerrainChunkMeshData
+    {
+        internal DigTerrainChunkMeshData(
+            Vector3[] vertices,
+            Vector3[] normals,
+            int[][] triangles,
+            DigTerrainMaterialKey[] materialKeys)
+        {
+            Vertices = vertices ?? throw new ArgumentNullException(nameof(vertices));
+            Normals = normals ?? throw new ArgumentNullException(nameof(normals));
+            Triangles = triangles ?? throw new ArgumentNullException(nameof(triangles));
+            MaterialKeys = materialKeys
+                ?? throw new ArgumentNullException(nameof(materialKeys));
+            if (vertices.Length != normals.Length)
+            {
+                throw new ArgumentException(
+                    "Terrain chunk vertices and normals must have equal lengths.");
+            }
+
+            if (triangles.Length != materialKeys.Length)
+            {
+                throw new ArgumentException(
+                    "Terrain chunk submeshes and material keys must have equal lengths.");
+            }
+        }
+
+        internal Vector3[] Vertices { get; }
+        internal Vector3[] Normals { get; }
+        internal int[][] Triangles { get; }
+        internal DigTerrainMaterialKey[] MaterialKeys { get; }
+
+        internal int TriangleCount
+        {
+            get
+            {
+                int count = 0;
+                for (int index = 0; index < Triangles.Length; index++)
+                {
+                    count += Triangles[index].Length / 3;
+                }
+
+                return count;
+            }
+        }
+    }
+}
