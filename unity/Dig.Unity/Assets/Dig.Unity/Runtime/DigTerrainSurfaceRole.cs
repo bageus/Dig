474ef49399626bbcd43c9c09c008fb1c94e4d1f1
bageus <36338626+bageus@users.noreply.github.com>
2026-07-20 01:24:15 +0200

    Add terrain profile and surface roles

---
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainSurfaceRole.cs
@@ -0,0 +1,20 @@
+namespace Dig.Unity
+{
+    public enum DigTerrainProfileKind
+    {
+        Sand = 0,
+        Stone = 1,
+        MetalBearing = 2,
+        Crystalline = 3,
+        Lava = 4,
+        Unmineable = 5,
+    }
+
+    public enum DigTerrainSurfaceRole
+    {
+        Floor = 0,
+        Wall = 1,
+        Ceiling = 2,
+        FreshCut = 3,
+    }
+}
