fff3a7e07ba746d61b4d3bf09b327b9ef73ad71b
bageus <36338626+bageus@users.noreply.github.com>
2026-07-19 21:03:20 +0200

    Add resident visual catalog type

---
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigResidentVisualCatalog.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Dig.Unity
+{
+    [CreateAssetMenu(
+        fileName = "ResidentVisualCatalog",
+        menuName = "Dig/Visual Catalogs/Residents")]
+    public sealed class DigResidentVisualCatalog : DigVisualCatalog
+    {
+    }
+}
