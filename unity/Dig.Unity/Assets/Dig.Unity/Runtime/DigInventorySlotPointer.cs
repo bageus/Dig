935b690c7743a660b863d5e716f5fe4e8dd78eeb
bageus <36338626+bageus@users.noreply.github.com>
2026-07-19 18:26:40 +0200

    Add inventory slot hover callbacks

--- unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigInventorySlotPointer.cs
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigInventorySlotPointer.cs
@@ -6,13 +6,28 @@ namespace Dig.Unity
 {
 
 [DisallowMultipleComponent]
-internal sealed class DigInventorySlotPointer : MonoBehaviour, IPointerClickHandler
+internal sealed class DigInventorySlotPointer : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerEnterHandler,
+    IPointerExitHandler
 {
     internal Action<PointerEventData>? Clicked { get; set; }
+    internal Action? Hovered { get; set; }
+    internal Action? Exited { get; set; }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Clicked?.Invoke(eventData);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Hovered?.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Exited?.Invoke();
     }
 }
 
