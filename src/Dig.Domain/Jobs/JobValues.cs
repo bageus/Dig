717c59e5b02edd9f78bad9c0d48caf85d5c0507e
bageus <36338626+bageus@users.noreply.github.com>
2026-07-20 16:36:27 +0200

    Add spatial job reservation keys

--- src/Dig.Domain/Jobs/JobValues.cs
+++ src/Dig.Domain/Jobs/JobValues.cs
@@ -111,7 +111,17 @@ public readonly struct ReservationKey : IEquatable<ReservationKey>, IComparable<
         return new ReservationKey(ReservationKind.Position, cellId.ToString());
     }
 
+    public static ReservationKey ForPosition(SpatialCellId cellId)
+    {
+        return new ReservationKey(ReservationKind.Position, cellId.ToString());
+    }
+
     public static ReservationKey ForDesignation(CellId cellId)
+    {
+        return new ReservationKey(ReservationKind.Designation, cellId.ToString());
+    }
+
+    public static ReservationKey ForDesignation(SpatialCellId cellId)
     {
         return new ReservationKey(ReservationKind.Designation, cellId.ToString());
     }
@@ -193,4 +203,5 @@ public sealed class JobBlockReason
         return $"{Code}: {Message}";
     }
 }
+
 }
