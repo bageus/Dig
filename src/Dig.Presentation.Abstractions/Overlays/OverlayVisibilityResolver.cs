eb2daa7021c6be3e7c77e747fc9b11a6026707e8
bageus <36338626+bageus@users.noreply.github.com>
2026-07-20 18:33:26 +0200

    Store overlay layer definitions

--- src/Dig.Presentation.Abstractions/Overlays/OverlayVisibilityResolver.cs
+++ src/Dig.Presentation.Abstractions/Overlays/OverlayVisibilityResolver.cs
@@ -1,10 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Dig.Presentation.Overlays
 {
 
 public sealed class OverlayVisibilityResolver
 {
+    private readonly Dictionary<OverlayLayerKind, OverlayLayerDefinition> _layers;
+
+    public OverlayVisibilityResolver(IEnumerable<OverlayLayerDefinition> layers)
+    {
+        if (layers is null)
+        {
+            throw new ArgumentNullException(nameof(layers));
+        }
+
+        _layers = layers.ToDictionary(value => value.Layer);
+        if (_layers.Count != Enum.GetValues(typeof(OverlayLayerKind)).Length)
+        {
+            throw new ArgumentException("Every overlay layer requires one definition.", nameof(layers));
+        }
+    }
+
+    public OverlayLayerDefinition ResolveLayer(OverlayLayerKind layer)
+    {
+        if (!_layers.TryGetValue(layer, out OverlayLayerDefinition? definition))
+        {
+            throw new ArgumentOutOfRangeException(nameof(layer));
+        }
+
+        return definition;
+    }
 }
 }
