fa2504c7a5b9942ad53efb1be7c46bb9615e261f
bageus <36338626+bageus@users.noreply.github.com>
2026-07-20 19:14:56 +0200

    Define cave preview box edges

--- unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigCaveRoomPreviewRenderer.Edges.cs
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigCaveRoomPreviewRenderer.Edges.cs
@@ -1,6 +1,26 @@
+using UnityEngine;
+
 namespace Dig.Unity
 {
     public sealed partial class DigCaveRoomPreviewRenderer
     {
+        private static Vector2Int[] CreateBoxConnections()
+        {
+            return new[]
+            {
+                new Vector2Int(0, 1),
+                new Vector2Int(1, 2),
+                new Vector2Int(2, 3),
+                new Vector2Int(3, 0),
+                new Vector2Int(4, 5),
+                new Vector2Int(5, 6),
+                new Vector2Int(6, 7),
+                new Vector2Int(7, 4),
+                new Vector2Int(0, 4),
+                new Vector2Int(1, 5),
+                new Vector2Int(2, 6),
+                new Vector2Int(3, 7),
+            };
+        }
     }
 }
