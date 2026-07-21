7d0abba2a5c501e13ddf7f0fe1f496dfd642a324
bageus <36338626+bageus@users.noreply.github.com>
2026-07-21 13:08:03 +0200

    Complete issue 212 URP and VFX pipeline

--- unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainChunkMeshData.cs
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainChunkMeshData.cs
@@ -8,18 +8,20 @@ namespace Dig.Unity
         internal DigTerrainChunkMeshData(
             Vector3[] vertices,
             Vector3[] normals,
+            Color[] colors,
             int[][] triangles,
             DigTerrainMaterialKey[] materialKeys)
         {
             Vertices = vertices ?? throw new ArgumentNullException(nameof(vertices));
             Normals = normals ?? throw new ArgumentNullException(nameof(normals));
+            Colors = colors ?? throw new ArgumentNullException(nameof(colors));
             Triangles = triangles ?? throw new ArgumentNullException(nameof(triangles));
             MaterialKeys = materialKeys
                 ?? throw new ArgumentNullException(nameof(materialKeys));
-            if (vertices.Length != normals.Length)
+            if (vertices.Length != normals.Length || vertices.Length != colors.Length)
             {
                 throw new ArgumentException(
-                    "Terrain chunk vertices and normals must have equal lengths.");
+                    "Terrain chunk vertices, normals and colors must have equal lengths.");
             }
 
             if (triangles.Length != materialKeys.Length)
@@ -31,6 +33,7 @@ namespace Dig.Unity
 
         internal Vector3[] Vertices { get; }
         internal Vector3[] Normals { get; }
+        internal Color[] Colors { get; }
         internal int[][] Triangles { get; }
         internal DigTerrainMaterialKey[] MaterialKeys { get; }
 
