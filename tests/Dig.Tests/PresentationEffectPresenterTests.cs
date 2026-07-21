562f3d04c5cefaa617b0deb43892a5d5a6be43a7
bageus <36338626+bageus@users.noreply.github.com>
2026-07-21 11:24:35 +0200

    Add presentation effect test shell

---
+++ tests/Dig.Tests/PresentationEffectPresenterTests.cs
@@ -0,0 +1,10 @@
+using Dig.Presentation.Rendering;
+using Xunit;
+
+namespace Dig.Tests
+{
+public sealed class PresentationEffectPresenterTests
+{
+    private readonly PresentationEffectPresenter _presenter = new PresentationEffectPresenter();
+}
+}
