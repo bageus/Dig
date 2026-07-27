fdb0ed515d8f4ac3e61f6a6a6f9b92e6edd8d903
bageus <36338626+bageus@users.noreply.github.com>
2026-07-27 21:12:24 +0200

    Split BuildingBox relocation preview boundary

--- src/Dig.Presentation.Abstractions/Buildings/BuildingBoxPlacementPresenter.cs
+++ src/Dig.Presentation.Abstractions/Buildings/BuildingBoxPlacementPresenter.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Dig.Domain.Buildings;
 using Dig.Domain.Content;
 using Dig.Domain.Core;
@@ -18,7 +17,7 @@ public static class BuildingBoxPreviewReasons
     public const string BoxUnavailable = "building_box.preview.box_unavailable";
 }
 
-public sealed class BuildingBoxPlacementPresenter
+public sealed partial class BuildingBoxPlacementPresenter
 {
     private static readonly DomainError InvalidPreview = new DomainError(
         "building_box.preview.invalid",
@@ -210,106 +209,6 @@ public sealed class BuildingBoxPlacementPresenter
             isValid: true,
             reasonCode: null,
             BuildingBoxPlacementKind.AssembleBuilding);
-    }
-
-    private static BuildingBoxGhostViewModel PreviewRelocation(
-        ItemStackSnapshot sourceStack,
-        BuildingDefinition definition,
-        CellId origin,
-        BuildingOrientation orientation,
-        WorldSnapshot world,
-        IReadOnlyCollection<CellId> occupiedCells,
-        IReadOnlyCollection<CellId> reachableCells)
-    {
-        CellId[] footprint = { origin };
-        if (!world.Size.Contains(origin))
-        {
-            return Invalid(
-                sourceStack.StackId,
-                definition,
-                origin,
-                orientation,
-                footprint,
-                BuildingErrors.PlacementOutOfBounds.Code,
-                BuildingBoxPlacementKind.RelocateBox,
-                isVisible: false);
-        }
-
-        CellSnapshot cell = world.Chunks
-            .SelectMany(chunk => chunk.Cells)
-            .First(value => value.Id == origin);
-        if (cell.IsSolid)
-        {
-            return Invalid(
-                sourceStack.StackId,
-                definition,
-                origin,
-                orientation,
-                footprint,
-                BuildingErrors.PlacementSolid.Code,
-                BuildingBoxPlacementKind.RelocateBox);
-        }
-
-        if (!cell.State.IsExplored)
-        {
-            return Invalid(
-                sourceStack.StackId,
-                definition,
-                origin,
-                orientation,
-                footprint,
-                BuildingErrors.PlacementUnexplored.Code,
-                BuildingBoxPlacementKind.RelocateBox,
-                isVisible: false);
-        }
-
-        if (!BuildingPlacementSurfaceFactProjector.HasSupportingPlane(origin, world))
-        {
-            return Invalid(
-                sourceStack.StackId,
-                definition,
-                origin,
-                orientation,
-                footprint,
-                PackableBuildingPlacementErrors.SurfaceMissing.Code,
-                BuildingBoxPlacementKind.RelocateBox,
-                isVisible: false);
-        }
-
-        if (occupiedCells.Contains(origin))
-        {
-            return Invalid(
-                sourceStack.StackId,
-                definition,
-                origin,
-                orientation,
-                footprint,
-                BuildingErrors.PlacementOccupied.Code,
-                BuildingBoxPlacementKind.RelocateBox);
-        }
-
-        if (!reachableCells.Contains(origin))
-        {
-            return Invalid(
-                sourceStack.StackId,
-                definition,
-                origin,
-                orientation,
-                footprint,
-                BuildingErrors.NoReachableWorkPosition.Code,
-                BuildingBoxPlacementKind.RelocateBox);
-        }
-
-        return new BuildingBoxGhostViewModel(
-            sourceStack.StackId,
-            definition.Id,
-            origin,
-            orientation,
-            footprint,
-            origin,
-            isValid: true,
-            reasonCode: null,
-            BuildingBoxPlacementKind.RelocateBox);
     }
 
     private static string? ValidateSource(
