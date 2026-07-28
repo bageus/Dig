069c3d0dcb3e1f122ac4a8350ae47de8b4080bac
bageus <36338626+bageus@users.noreply.github.com>
2026-07-28 17:51:37 +0200

    Add agent runtime and active meal save contracts

---
+++ src/Dig.Application/Saving/AgentRuntimeSaveData.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Dig.Application.Saving
+{
+
+[DataContract]
+public sealed class AgentRuntimeSaveData
+{
+    [DataMember(Order = 1)]
+    public List<AgentRuntimeStateSaveData> Agents { get; set; } =
+        new List<AgentRuntimeStateSaveData>();
+}
+
+[DataContract]
+public sealed class AgentRuntimeStateSaveData
+{
+    [DataMember(Order = 1)] public string AgentId { get; set; } = string.Empty;
+    [DataMember(Order = 2)] public int Nutrition { get; set; }
+    [DataMember(Order = 3)] public int Alertness { get; set; }
+    [DataMember(Order = 4)] public int Mood { get; set; }
+    [DataMember(Order = 5)] public int Health { get; set; }
+    [DataMember(Order = 6)] public long LastNeedsTick { get; set; } = -1;
+    [DataMember(Order = 7, EmitDefaultValue = false)]
+    public ActiveFoodMealSaveData? ActiveMeal { get; set; }
+}
+
+[DataContract]
+public sealed class ActiveFoodMealSaveData
+{
+    [DataMember(Order = 1)] public string SourceStackId { get; set; } = string.Empty;
+    [DataMember(Order = 2)] public string ItemId { get; set; } = string.Empty;
+    [DataMember(Order = 3)] public int TotalNutrition { get; set; }
+    [DataMember(Order = 4)] public int BiteCount { get; set; }
+    [DataMember(Order = 5)] public int CompletedBites { get; set; }
+    [DataMember(Order = 6)] public long StartedTick { get; set; }
+}
+
+}
