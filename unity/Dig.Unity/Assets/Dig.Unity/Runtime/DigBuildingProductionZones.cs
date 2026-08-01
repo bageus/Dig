4a46fb43b6430ec9c659a3c07577e6f718be3d0e
bageus <36338626+bageus@users.noreply.github.com>
2026-08-01 10:10:44 +0200

    Fix production package content namespace import

--- unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigBuildingProductionZones.cs
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigBuildingProductionZones.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Dig.Application.Production;
 using Dig.Domain.Buildings;
+using Dig.Domain.Content;
 using Dig.Domain.Core;
 using Dig.Domain.Inventory;
 using Dig.Domain.Jobs;
