f0c458d51345f90afae632e38c6e5d4e59e5d46b
bageus <36338626+bageus@users.noreply.github.com>
2026-08-01 10:11:12 +0200

    Fix production package route plan namespace import

--- unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainWorkSession.ProductionPackages.cs
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainWorkSession.ProductionPackages.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dig.Application.Navigation;
 using Dig.Application.Production;
 using Dig.Domain.Core;
 using Dig.Domain.Inventory;
