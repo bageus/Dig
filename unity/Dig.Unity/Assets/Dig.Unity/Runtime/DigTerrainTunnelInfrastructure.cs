876755579d59b1d1eb5df21024f35e5f0ee35b8a
bageus <36338626+bageus@users.noreply.github.com>
2026-08-03 16:36:55 +0200

    Fix tunnel infrastructure Unity job namespace

--- unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainTunnelInfrastructure.cs
+++ unity/Dig.Unity/Assets/Dig.Unity/Runtime/DigTerrainTunnelInfrastructure.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dig.Application.Jobs;
 using Dig.Application.Tunnels;
 using Dig.Domain.Buildings;
 using Dig.Domain.Core;
