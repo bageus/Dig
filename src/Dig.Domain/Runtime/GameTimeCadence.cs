90594df5c0c14aaf2185f41f9067829228878479
bageus <36338626+bageus@users.noreply.github.com>
2026-08-03 23:52:54 +0200

    Make real-to-game coefficient explicit

--- src/Dig.Domain/Runtime/GameTimeCadence.cs
+++ src/Dig.Domain/Runtime/GameTimeCadence.cs
@@ -3,14 +3,53 @@ using System;
 namespace Dig.Domain.Runtime
 {
 
+public readonly struct GameTimeSnapshot
+{
+    public GameTimeSnapshot(
+        long totalGameSeconds,
+        long dayIndex,
+        int hour,
+        int minute,
+        int second,
+        int tickOfDay)
+    {
+        TotalGameSeconds = totalGameSeconds;
+        DayIndex = dayIndex;
+        Hour = hour;
+        Minute = minute;
+        Second = second;
+        TickOfDay = tickOfDay;
+    }
+
+    public long TotalGameSeconds { get; }
+
+    public long DayIndex { get; }
+
+    public int Hour { get; }
+
+    public int Minute { get; }
+
+    public int Second { get; }
+
+    public int TickOfDay { get; }
+}
+
 public static class GameTimeCadence
 {
+    public const int GameSecondsPerMinute = 60;
+    public const int GameMinutesPerHour = 60;
     public const int HoursPerDay = 24;
-    public const int TicksPerHour = 150;
-    public const int TicksPerDay = HoursPerDay * TicksPerHour;
-    public const int GameSecondsPerTick = 24;
+    public const int GameSecondsPerHour = GameSecondsPerMinute * GameMinutesPerHour;
+    public const int GameSecondsPerDay = GameSecondsPerHour * HoursPerDay;
 
-    public static readonly TimeSpan NormalTickDuration = TimeSpan.FromSeconds(1);
+    public const int RealMillisecondsPerNormalTick = 1_000;
+    public const int GameSecondsPerRealSecond = 24;
+    public const int GameSecondsPerTick = GameSecondsPerRealSecond;
+    public const int TicksPerHour = GameSecondsPerHour / GameSecondsPerTick;
+    public const int TicksPerDay = GameSecondsPerDay / GameSecondsPerTick;
+
+    public static readonly TimeSpan NormalTickDuration =
+        TimeSpan.FromMilliseconds(RealMillisecondsPerNormalTick);
 
     public static long TicksFromHours(int hours)
     {
@@ -32,14 +71,56 @@ public static class GameTimeCadence
         return checked((long)days * TicksPerDay);
     }
 
-    public static int HourOfDay(long tick)
+    public static long TotalGameSeconds(long tick)
     {
         if (tick < 0)
         {
             throw new ArgumentOutOfRangeException(nameof(tick));
         }
 
-        return (int)((tick % TicksPerDay) / TicksPerHour);
+        return checked(tick * GameSecondsPerTick);
+    }
+
+    public static int TickOfDay(long tick)
+    {
+        if (tick < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(tick));
+        }
+
+        return (int)(tick % TicksPerDay);
+    }
+
+    public static int HourOfDay(long tick)
+    {
+        return Project(tick).Hour;
+    }
+
+    public static GameTimeSnapshot Project(long tick)
+    {
+        long totalGameSeconds = TotalGameSeconds(tick);
+        long dayIndex = totalGameSeconds / GameSecondsPerDay;
+        int secondOfDay = (int)(totalGameSeconds % GameSecondsPerDay);
+        int hour = secondOfDay / GameSecondsPerHour;
+        int minute = (secondOfDay % GameSecondsPerHour) / GameSecondsPerMinute;
+        int second = secondOfDay % GameSecondsPerMinute;
+        return new GameTimeSnapshot(
+            totalGameSeconds,
+            dayIndex,
+            hour,
+            minute,
+            second,
+            TickOfDay(tick));
+    }
+
+    public static int EffectiveGameSecondsPerRealSecond(int playbackMultiplier)
+    {
+        if (playbackMultiplier < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(playbackMultiplier));
+        }
+
+        return checked(GameSecondsPerRealSecond * playbackMultiplier);
     }
 }
 
