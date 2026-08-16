3526a6d28617655520dc1bcd33a5f21e9a7be01e
bageus <36338626+bageus@users.noreply.github.com>
2026-08-16 11:00:20 +0200

    fix(farm): import inventory repository contract

--- src/Dig.Application/Farming/FarmOutputLogisticsUseCases.cs
+++ src/Dig.Application/Farming/FarmOutputLogisticsUseCases.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dig.Application.Inventory;
 using Dig.Application.Jobs;
 using Dig.Application.Messaging;
 using Dig.Domain.Core;
